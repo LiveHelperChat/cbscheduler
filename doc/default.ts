bff45477e35bfc5514036121a86e2d2c5650d5ab
Remigijus Kiminas <remdex@gmail.com>
2020-09-24 05:27:48 -0400

    Default translations

---
+++ doc/default.ts
@@ -0,0 +1,506 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="lt">
+  <context>
+    <name>module/cbscheduler</name>
+    <message>
+      <source>We will show modal window to schedule a call!</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Please enter a name!</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Please enter a schedule name!</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Please choose a time zone!</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Today</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Tomorrow</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Please enter a valid email address</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Please enter a username</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Please choose a subject</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Department is missing</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Please enter a phone</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Invalid phone number!</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Your phone number does not look as valid phone number!</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Please enter a description</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Please choose a day</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Please choose a time</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>You have already a call scheduled for</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>between</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>and</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Slot became unavailable. Please choose another time.</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Monday</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Tuesday</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Wednesday</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Thursday</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Friday</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Saturday</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Sunday</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Scheduled</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Completed</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Canceled</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Serviced by</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Assign to me</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Edit</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Edit reservation</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Callback scheduler</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Reservations</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Options</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Schedules</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Subjects</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>New schedule</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>New subject</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Callback Scheduler Options</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Block phone numbers. Wildcard at the phone number is also available.</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Allow numbers. Wildcard at the phone number is also available.</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>How many days upfront allow to choose?</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Minimum time in minutes before next available slot?</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Name</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Active</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Time Zone of the schedule</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Choose Time Zone</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Add</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Start time</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>End time</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Max Calls</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Phone</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Username</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>E-mail</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Scheduled for</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Subject</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Department</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Previous call</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Chat ID</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Description</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Status</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Outcome of the call</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Present outcome of the call</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Follow up</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Are you sure you want to schedule this call again?</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Position</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Preview call</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>ID</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Agent</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Phone number</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>scheduled for</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>User timezone</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>New</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Your scheduled telephone callback request</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Username*</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>E-mail*</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Phone*</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Day</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Problem description*</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Enter phone number*</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Choose a day</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Schedule a callback</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Choose a time</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Time</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Please include country code in your phone number.</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Choose a subject*</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Thank you. Your call has been scheduled. You can add it to your calendar by clicking</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>here</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Choose day and time we should call you. Times are represented in {{timezone}} timezone.</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>At the moment we do not have any free slots for a callback.</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Live support</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Try again</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>My username and e-mail verification failed to schedule a call.</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Close</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Loading...</source>
+      <translation type="unfinished"/>
+    </message>
+  </context>
+  <context>
+    <name>system/buttons</name>
+    <message>
+      <source>Save</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Update</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Cancel</source>
+      <translation type="unfinished"/>
+    </message>
+  </context>
+  <context>
+    <name>user/account</name>
+    <message>
+      <source>Updated</source>
+      <translation type="unfinished"/>
+    </message>
+  </context>
+  <context>
+    <name>chat/onlineusers</name>
+    <message>
+      <source>Settings updated</source>
+      <translation type="unfinished"/>
+    </message>
+  </context>
+  <context>
+    <name>chat/lists/search_panel</name>
+    <message>
+      <source>Choose department</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Choose department group</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Username</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Status</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Agent</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Agent group</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Department</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Department group</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Date range from</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Set filter to today</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Hour and minute from</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Date range to</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Hour and minute to</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Search</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Select agent</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Select group</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Select department</source>
+      <translation type="unfinished"/>
+    </message>
+  </context>
+  <context>
+    <name>kernel/messages</name>
+    <message>
+      <source>Are you sure?</source>
+      <translation type="unfinished"/>
+    </message>
+  </context>
+</TS>
