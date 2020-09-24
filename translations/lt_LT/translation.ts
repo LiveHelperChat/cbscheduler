47fb2bdcaeb9a2627088508ed1fcf93c97a6d01e
Remigijus Kiminas <remdex@gmail.com>
2020-09-24 05:36:44 -0400

    Add translation folder

---
+++ translations/lt_LT/translation.ts
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="lt">
+    <context>
+        <name>module/cbscheduler</name>
+    <message>
+        <source>Callback scheduler</source>
+        <translation>Paskambinimo užsakymas</translation>
+    </message>
+</context>
+</TS>
