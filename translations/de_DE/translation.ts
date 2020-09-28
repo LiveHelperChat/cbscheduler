c6674c83aa404d906973194d42dd057759dd8956
Remigijus Kiminas <remdex@gmail.com>
2020-09-28 02:57:32 -0400

    Add german translations

---
+++ translations/de_DE/translation.ts
@@ -0,0 +1,428 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="de">
+  <context>
+    <name>module/cbscheduler</name>
+    <message>
+      <source>We will show modal window to schedule a call!</source>
+      <translation>Wir zeigen ein modales Fenster, um einen Anruf zu planen!</translation>
+    </message>
+    <message>
+      <source>Please enter a name!</source>
+      <translation>Bitte geben Sie einen Namen ein!</translation>
+    </message>
+    <message>
+      <source>Please enter a schedule name!</source>
+      <translation>Bitte geben Sie einen Zeitplannamen ein!</translation>
+    </message>
+    <message>
+      <source>Please choose a time zone!</source>
+      <translation>Bitte wählen Sie eine Zeitzone!</translation>
+    </message>
+    <message>
+      <source>Today</source>
+      <translation>Heute</translation>
+    </message>
+    <message>
+      <source>Tomorrow</source>
+      <translation>Morgen</translation>
+    </message>
+    <message>
+      <source>Please enter a valid email address</source>
+      <translation>Bitte geben Sie eine gültige E-Mail-Adresse ein</translation>
+    </message>
+    <message>
+      <source>Please enter a username</source>
+      <translation>Bitte geben Sie einen Benutzernamen ein</translation>
+    </message>
+    <message>
+      <source>Please choose a subject</source>
+      <translation>Bitte wählen Sie ein Thema</translation>
+    </message>
+    <message>
+      <source>Department is missing</source>
+      <translation>Abteilung fehlt</translation>
+    </message>
+    <message>
+      <source>Please enter a phone</source>
+      <translation>Bitte geben Sie ein Telefon ein</translation>
+    </message>
+    <message>
+      <source>Invalid phone number!</source>
+      <translation>Ungültige Telefonnummer!</translation>
+    </message>
+    <message>
+      <source>Your phone number does not look as valid phone number!</source>
+     <translation>Ihre Telefonnummer sieht nicht als gültige Telefonnummer aus!</translation>
+    </message>
+    <message>
+      <source>Please enter a description</source>
+     <translation>Bitte geben Sie eine Beschreibung ein</translation>
+    </message>
+    <message>
+      <source>Please choose a day</source>
+     <translation>Bitte wählen Sie einen Tag</translation>
+    </message>
+    <message>
+      <source>Please choose a time</source>
+      <translation>Bitte wählen Sie eine Zeit</translation>
+    </message>
+    <message>
+      <source>You have already a call scheduled for</source>
+      <translation>Sie haben bereits einen Anruf für geplant</translation>
+    </message>
+    <message>
+      <source>between</source>
+     <translation>zwischen</translation>
+    </message>
+    <message>
+      <source>and</source>
+      <translation>und</translation>
+    </message>
+    <message>
+      <source>Slot became unavailable. Please choose another time.</source>
+      <translation>Slot wurde nicht mehr verfügbar. Bitte wählen Sie eine andere Zeit.</translation>
+    </message>
+    <message>
+      <source>Monday</source>
+     <translation>Monday</translation>
+    </message>
+    <message>
+      <source>Tuesday</source>
+     <translation>Dienstag</translation>
+    </message>
+    <message>
+      <source>Wednesday</source>
+      <translation>Mittwoch</translation>
+    </message>
+    <message>
+      <source>Thursday</source>
+      <translation>Donnerstag</translation>
+    </message>
+    <message>
+      <source>Friday</source>
+      <translation>Freitag</translation>
+    </message>
+    <message>
+      <source>Saturday</source>
+      <translation>Samstag</translation>
+    </message>
+    <message>
+      <source>Sunday</source>
+    <translation>Sonntag</translation>
+    </message>
+    <message>
+      <source>Scheduled</source>
+      <translation>Geplant</translation>
+    </message>
+    <message>
+      <source>Completed</source>
+      <translation>Abgeschlossen</translation>
+    </message>
+    <message>
+      <source>Canceled</source>
+      <translation>Abgesagt</translation>
+    </message>
+    <message>
+      <source>Serviced by</source>
+      <translation>Von gewartet</translation>
+    </message>
+    <message>
+      <source>Assign to me</source>
+      <translation>Weisen Sie mir zu</translation>
+    </message>
+    <message>
+      <source>Edit</source>
+      <translation>Bearbeiten</translation>
+    </message>
+    <message>
+      <source>Edit reservation</source>
+      <translation>Reservierung bearbeiten</translation>
+    </message>
+    <message>
+      <source>Callback scheduler</source>
+     <translation>Rückrufplaner</translation>>
+    </message>
+    <message>
+      <source>Reservations</source>
+      <translation>Reservierungen</translation>
+    </message>
+    <message>
+      <source>Options</source>
+      <translation>Optionen</translation>
+    </message>
+    <message>
+      <source>Schedules</source>
+      <translation>Zeitpläne</translation>
+    </message>
+    <message>
+      <source>Subjects</source>
+      <translation>Fächer</translation>
+    </message>
+    <message>
+      <source>New schedule</source>
+      <translation>Neuer Zeitplan</translation>
+    </message>
+    <message>
+      <source>New subject</source>
+      <translation>Neues Thema</translation>
+    </message>
+    <message>
+      <source>Callback Scheduler Options</source>
+      <translation>Callback Scheduler-Optionen</translation>
+    </message>
+    <message>
+      <source>Block phone numbers. Wildcard at the phone number is also available.</source>
+      <translation>Telefonnummern blockieren. Ein Platzhalter unter der Telefonnummer ist ebenfalls verfügbar.</translation>
+    </message>
+    <message>
+      <source>Allow numbers. Wildcard at the phone number is also available.</source>
+      <translation>Zahlen zulassen. Ein Platzhalter unter der Telefonnummer ist ebenfalls verfügbar.</translation>
+    </message>
+    <message>
+      <source>How many days upfront allow to choose?</source>
+      <translation>Wie viele Tage im Voraus können Sie wählen?</translation>
+    </message>
+    <message>
+      <source>Minimum time in minutes before next available slot?</source>
+      <translation>Mindestzeit in Minuten vor dem nächsten verfügbaren Slot?</translation>
+    </message>
+    <message>
+      <source>Name</source>
+      <translation>Name</translation>
+    </message>
+    <message>
+      <source>Active</source>
+      <translation>Aktiv</translation>>
+    </message>
+    <message>
+      <source>Time Zone of the schedule</source>
+     <translation>Zeitzone des Zeitplans</translation>
+    </message>
+    <message>
+      <source>Choose Time Zone</source>
+      <translation>Wählen Sie Zeitzone</translation>
+    </message>
+    <message>
+      <source>Add</source>
+      <translation>CAdd</translation>
+    </message>
+    <message>
+      <source>Start time</source>
+      <translation>Startzeit</translation>
+    </message>
+    <message>
+      <source>End time</source>
+      <translation>Endzeit</translation>
+    </message>
+    <message>
+      <source>Max Calls</source>
+      <translation>Max Anrufe</translation>
+    </message>
+    <message>
+      <source>Phone</source>
+      <translation>Telefon</translation>
+    </message>
+    <message>
+      <source>Username</source>
+      <translation>Nutzername</translation>
+    </message>
+    <message>
+      <source>E-mail</source>
+      <translation>Email</translation>
+    </message>
+    <message>
+      <source>Scheduled for</source>
+      <translation>Eingetragen für</translation>
+    </message>
+    <message>
+      <source>Subject</source>
+      <translation>Gegenstand </translation>
+    </message>
+    <message>
+      <source>Department</source>
+      <translation>Abteilung</translation>
+    </message>
+    <message>
+      <source>Previous call</source>
+      <translation>Vorheriger Anruf</translation>
+    </message>
+    <message>
+      <source>Chat ID</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Description</source>
+      <translation>Beschreibung</translation>
+    </message>
+    <message>
+      <source>Status</source>
+      <translation>Status</translation>
+    </message>
+    <message>
+      <source>Outcome of the call</source>
+     <translation>Ergebnis des Anrufs</translation>
+    </message>
+    <message>
+      <source>Present outcome of the call</source>
+      <translation>Aktuelles Ergebnis des Anrufs</translation>
+    </message>
+    <message>
+      <source>Follow up</source>
+      <translation>Nachverfolgen</translation>
+    </message>
+    <message>
+      <source>Are you sure you want to schedule this call again?</source>
+      <translation>Möchten Sie diesen Anruf wirklich erneut planen?</translation>
+    </message>
+    <message>
+      <source>Position</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Preview call</source>
+      <translation>Preview call</translation>
+    </message>
+    <message>
+      <source>ID</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Agent</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Phone number</source>
+      <translation>Telefonnummer</translation>
+    </message>
+    <message>
+      <source>scheduled for</source>
+      <translation>eingetragen für</translation>
+    </message>
+    <message>
+      <source>User timezone</source>
+      <translation>Benutzerzeitzone</translation>
+    </message>
+    <message>
+      <source>New</source>
+      <translation>Neu</translation>
+    </message>
+    <message>
+      <source>Your scheduled telephone callback request</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Username*</source>
+      <translation>Nutzername*</translation>
+    </message>
+    <message>
+      <source>E-mail*</source>
+      <translation>E-mail*</translation>
+    </message>
+    <message>
+      <source>Phone*</source>
+      <translation>Telefon*</translation>
+    </message>
+    <message>
+      <source>Day</source>
+      <translation>Tag</translation>
+    </message>
+    <message>
+      <source>Problem description*</source>
+      <translation>Problembeschreibung*</translation>
+    </message>
+    <message>
+      <source>Enter phone number*</source>
+      <translation>Telefonnummer eingeben*</translation>
+    </message>
+    <message>
+      <source>Choose a day</source>
+      <translation>Wähle einen Tag</translation>
+    </message>
+    <message>
+      <source>Schedule a callback</source>
+      <translation>Planen Sie einen Rückruf</translation>>
+    </message>
+    <message>
+      <source>Choose a time</source>
+      <translation>Wähle eine Zeit</translation>
+    </message>
+    <message>
+      <source>Time</source>
+      <translation>Zeit</translation>
+    </message>
+    <message>
+      <source>Please include country code in your phone number.</source>
+      <translation>Bitte geben Sie die Landesvorwahl in Ihrer Telefonnummer an.</translation>
+    </message>
+    <message>
+      <source>Choose a subject*</source>
+      <translation>Wählen Sie ein Thema *</translation>
+    </message>
+    <message>
+      <source>Thank you. Your call has been scheduled. You can add it to your calendar by clicking</source>
+      <translation>Dankeschön. Ihr Anruf wurde geplant. Sie können es Ihrem Kalender hinzufügen, indem Sie auf klicken</translation>
+    </message>
+    <message>
+      <source>here</source>
+     <translation>Hier</translation>
+    </message>
+    <message>
+      <source>Choose day and time we should call you. Times are represented in {{timezone}} timezone.</source>
+      <translation>Choose day and time we should call you. Times are represented in {{timezone}} Zeitzone. </translation>
+    </message>
+    <message>
+      <source>At the moment we do not have any free slots for a callback.</source>
+      <translation>Im Moment haben wir keine freien Plätze für einen Rückruf.</translation>
+    </message>
+    <message>
+      <source>Live support</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Try again</source>
+      <translation>Versuchen Sie es nochmal</translation>
+    </message>
+    <message>
+      <source>My username and e-mail verification failed to schedule a call.</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Close</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Loading...</source>
+      <translation type="unfinished"/>
+    </message>
+  </context>
+  <context>
+    <name>system/buttons</name>
+    <message>
+      <source>Save</source>
+      <translation>speichern</translation>
+    </message>
+    <message>
+      <source>Update</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Cancel</source>
+      <translation type="unfinished"/>
+    </message>
+  </context>
+  <context>
+    <name>user/account</name>
+    <message>
+      <source>Updated</source>
+      <translation type="unfinished"/>
+    </message>
+  </context>
+  <context>
+    <name>chat/onlineusers</name>
+    <message>
+      <source>Settings updated</source>
+      <translation>Einstellungen aktualisiert</translation>
+    </message>
+  </context>
+</TS>
