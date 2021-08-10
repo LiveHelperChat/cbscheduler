9373ee8683148c9c4ce947fa13976b45d0fca7dd
Remigijus Kiminas <remdex@gmail.com>
2021-08-10 09:19:12 -0400

    Correct place for override

--- design/cbschedulertheme/tpl/pagelayouts/parts/page_head_js_extension_multiinclude.tpl.php
+++ design/cbschedulertheme/tpl/pagelayouts/parts/page_head_js_extension_multiinclude.tpl.php
@@ -1,7 +1,1 @@
-<?php 
-/**
- * Override this template
- * 
- * <script type="text/javascript" src="<?php echo erLhcoreClassDesign::designJS('js/extension.lhc.js');?>"></script>
- * */
-?>
+<script type="text/javascript" src="<?php echo erLhcoreClassDesign::designJS('js/cbscheduler.js');?>"></script>
