3740beb465e39fd30f5c0132957156845f356111
Remigijus Kiminas <remdex@gmail.com>
2022-08-30 08:04:56 -0400

    Cleanup

--- design/cbschedulertheme/js/scheduler/src/components/parts/ChooseTZModule.js
+++ design/cbschedulertheme/js/scheduler/src/components/parts/ChooseTZModule.js
@@ -10,67 +10,6 @@ const ChooseTZModule = props => {
     const [isLoaded, setLoaded] = useState(false);
     const [timeZones, setTimeZones] = useState([]);
 
-    /*const [phone, setPhone] = useState((props.phone != '' && props.phone != null) ? props.phone : null);
-    const [email, setEmail] = useState((props.email != '' && props.email != null) ? props.email : null);
-    const [username, setUsername] = useState((props.username != '' && props.username != null && props.username != 'Visitor' && props.username != 'undefined') ? props.username : null);
-
-    const [isSubmitting, setSubmitting] = useState(false);
-    const [isDisabled, setDisabled] = useState(false);
-    const [isScheduled, setScheduled] = useState(false);
-    const [cbData, setData] = useState(null);
-
-    // Error messages
-    const [errors, setErrors] = useState([]);
-
-    // Error message
-    const [error, setError] = useState(null);
-    const [error_code, setErrorCode] = useState(null);
-
-    const cancelScheduled = () => {
-        props.setCancelMode(false);
-    }
-
-    const getPostData = () => {
-        return {
-            'username':username,
-            'timezone':props.timezone,
-            'phone':phone,
-            'email':email,
-            'dep_id':props.dep_id,
-            'chat_id':props.chat_id,
-            'hash':props.hash
-        };
-    }
-
-    const cancelCallbackAction = () => {
-        setSubmitting(true);
-        axios.post(props.base_path  + "cbscheduler/cancelschedulecb", (typeof postData !== 'undefined' ? postData : getPostData())).then(result => {
-            setErrors([]);
-            setError(null);
-            setErrorCode(null);
-
-            if (result.data.error == true) {
-                setSubmitting(false);
-
-                if (result.data.messages) {
-                    setErrors(result.data.messages);
-                }
-
-                if (result.data.code) {
-                    setErrorCode(result.data.code);
-                }
-
-                if (result.data.message) {
-                    setError(result.data.message);
-                }
-
-            } else {
-                setData(result.data.data);
-                setScheduled(true);
-            }
-        });
-    }*/
-
     const getTimeZones = () => {
         axios.get(props.base_path  + "cbscheduler/gettz" ).then(result => {
             setTimeZones(result.data);
