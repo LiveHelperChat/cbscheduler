83a9ebf416c4f9f7641dfe9246eb1434baa12e92
Michael Meneses <michael@middag.com.br>
2023-10-19 20:32:08 -0300

    FIX JSON_CONTAINS for dep_id

--- classes/erlhcoreclassmodelcbschedulerreservation.php
+++ classes/erlhcoreclassmodelcbschedulerreservation.php
@@ -95,7 +95,7 @@ class erLhcoreClassModelCBSchedulerReservation
 
                 $db = ezcDbInstance::get();
 
-                $phoneTransform = erLhcoreClassModelCBSchedulerPhoneTransform::findOne(array('customfilter' => array('(JSON_CONTAINS(`country`,'.$db->quote('"'.$region.'"').',\'$\') AND JSON_CONTAINS(`dep_id`,'. (int)$this->dep_id . ',\'$\') )')));
+                $phoneTransform = erLhcoreClassModelCBSchedulerPhoneTransform::findOne(array('customfilter' => array('(JSON_CONTAINS(`country`,'.$db->quote('"'.$region.'"').',\'$\') AND JSON_CONTAINS(`dep_id`,'. $db->quote('"'.(int)$this->dep_id.'"') . ',\'$\') )')));
 
                 if (!($phoneTransform instanceof erLhcoreClassModelCBSchedulerPhoneTransform)) {
                     $phoneTransform = erLhcoreClassModelCBSchedulerPhoneTransform::findOne(array('customfilter' => array('(`country` = \'[]\' AND JSON_CONTAINS(`dep_id`,'. (int)$this->dep_id . ',\'$\') )')));
